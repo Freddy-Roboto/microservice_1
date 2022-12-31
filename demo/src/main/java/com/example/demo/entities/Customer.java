7807581a89149dcbbaea65a587e622491f599079
Freddy-Roboto <freddytenetorres@gmail.com>
2022-12-31 11:09:29 -0500

    add to github

---
+++ demo/src/main/java/com/example/demo/entities/Customer.java
@@ -0,0 +1,19 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+@Entity
+@Table(name = "customers")
+@Getter @Setter
+@ToString
+@EqualsAndHashCode
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String texto;
+}
